cb038cfb3cd349f84bde672af72a56227b5b841b
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-11 20:09:59 -0400

    Added funtionality to create buttons and then recall information from buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 //Global Variables
-const apiKey = "118087c4eee9cfc3b0ed1bace9c90b04"
+const apiKey = "118087c4eee9cfc3b0ed1bace9c90b04";
 
 //Script
 //Adds an object of the city name and coordinates to local storage
@@ -7,7 +7,7 @@ function addCity(event) {
     //Prevents default of form submission
     event.preventDefault();
     //Pulls an array of city objects from local storage
-    let storedCities = new Array (JSON.parse(localStorage.getItem("cities")));
+    let storedCities = JSON.parse(localStorage.getItem("cities"));
 
     //Pull seached city from input and does a null check
     var inputField = document.getElementById("citySearch");
@@ -28,7 +28,7 @@ function addCity(event) {
             if (!response.ok) {
                 alert("Could not find city, please try again");
                 return;
-            }
+            };
 
             return response.json();
         })
@@ -39,11 +39,12 @@ function addCity(event) {
             };
 
             //Creates an object for the city
-            let cityObject = { Name: data[0].name, Lat: data[0].lat, Lon: data[0].lon};
+            let cityObject = {Name: data[0].name, Lat: data[0].lat, Lon: data[0].lon};
 
             //Checks if any cities are stored in local storage if no makes object first in the array
-            if (!storedCities[0]) {
-                localStorage.setItem("cities", JSON.stringify(cityObject));
+            if (!storedCities) {
+                let firstCity = new Array(cityObject);
+                localStorage.setItem("cities", JSON.stringify(firstCity));
             } else {
                 //If there were saved cities, it adds the city object to the array and resaves
                 storedCities.push(cityObject);
@@ -53,15 +54,52 @@ function addCity(event) {
             //Runs function to make buttons
             populateCities();
         })
-}
+};
 
 function populateCities() {
 
-    const myNode = document.getElementById("cityBtns");
+    let storedCities = JSON.parse(localStorage.getItem("cities"));
 
-    myNode.replaceChildren();
-}
+    //Clears all buttons from list
+    var e = document.querySelector("nav");
+    var child = e.lastElementChild; 
+    while (child) {
+        e.removeChild(child);
+        child = e.lastElementChild;
+    };
 
-//TODO Clear all buttons of saved cities
-//TODO Populate city buttons
-document.getElementById("searchBtn").addEventListener("click", addCity)
+        // Loop to generate new list of buttons and display them on page
+    if (storedCities) {
+        for (let i=0; i < storedCities.length; i++) {
+        const newBtn = document.createElement("button");
+        let cityBtnName = storedCities[i].Name;
+        //Sets the new button to have the text of the cities name
+        newBtn.innerHTML = cityBtnName;
+        //sets a data number to be used later when pulling weather
+        newBtn.dataset.arrayNum = i;
+        //Appends to page
+        document.getElementById("cityBtns").appendChild(newBtn);
+        };
+    };
+};
+
+//Takes the array number from the button to search then display weather data
+function displayWeather (event) {
+    // Checks that a button in the nav was pressed and not the nav itself or another element
+    let selectedBtn = event.target.closest('button');
+    if (!selectedBtn) return;
+    // Looks up the array number saved as a data property in the element
+    let cityNum = selectedBtn.dataset.arrayNum
+    let storedCities = JSON.parse(localStorage.getItem("cities"));
+    let cityInfo = storedCities[cityNum]
+
+};
+
+// Todo pull city from button clicks
+// Todo pull API data from stored logitude and latitude
+// Todo place on page
+
+populateCities();
+
+document.getElementById("searchBtn").addEventListener("click", addCity);
+document.getElementById("cityBtns").onclick = displayWeather;
